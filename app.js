8ab0caa891ef993a0e5d9cbc4c3b3796e9c50548
fishyboi@MSI <fishyboi@MSI>
2023-08-29 19:48:36 -0400

    Several hours of forgetting to defer

--- app.js
+++ app.js
@@ -3,14 +3,33 @@ const state = {
     selectedCharacter: null
 }
 
+const charDiv = document.getElementById("charDiv");
+console.log(charDiv);
+
 const charAllDiv = document.getElementById("charAllDiv");
+console.log(charAllDiv);
+
+function renderCharacterList() {
+    const allChar = state.characters.map((character) => {
+        return `
+        <div><a href=#${character.name}> ${character.name} </a></div>
+        `
+    })
+    charAllDiv.innerHTML = allChar.join('');
+}
 
 async function fetchCharacterList(){
     const info = await fetch("https://swapi.dev/api/people/?page=2");
     const holocron = await info.json();
     console.log(holocron.results);
     state.characters = holocron.results;
-    console.log(state);
+    console.log("state -->", state);
 }
 
-fetchCharacterList();
+async function render(){
+    await fetchCharacterList()
+    renderCharacterList()
+    
+}
+
+render()
