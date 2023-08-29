955bbd2c11b52958f8dbc2ca346622dac91185c6
fishyboi@MSI <fishyboi@MSI>
2023-08-29 17:09:55 -0400

    Character List

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-console.log("Hello There");
+const state = {
+    characters: [],
+    selectedCharacter: null
+}
+
+const charAllDiv = document.getElementById("charAllDiv");
+
+async function fetchCharacterList(){
+    const info = await fetch("https://swapi.dev/api/people/?page=2");
+    const holocron = await info.json();
+    console.log(holocron.results);
+    state.characters = holocron.results;
+    console.log(state);
+}
+
+fetchCharacterList();
